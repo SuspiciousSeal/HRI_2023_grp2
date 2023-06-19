345d5b1e0107ddd7476d2382e180c401adea391d
Robert Allik <robert.allik@protonmail.com>
2023-06-19 14:35:15 +0300

    added tts to follow_tag

--- src/ar_tag_tracker/src/follow_tag.cpp
+++ src/ar_tag_tracker/src/follow_tag.cpp
@@ -8,13 +8,19 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include "geometry_msgs/Twist.h"
+#include "std_msgs/String.h"
 
 ros::Publisher velocity_pub;
+ros::Publisher tts_pub;
 auto arposecallback(ar_track_alvar_msgs::AlvarMarkers markers)
 { 
     if (markers.markers.size() > 0)
     {
         auto marker = markers.markers.front();
+        std_msgs::String tts_msg;
+        tts_msg.data = "Got marker ";
+        tts_msg.data.append(std::to_string(marker.id));
+        tts_pub.publish(tts_msg);
         auto pos = marker.pose.pose.position;
         
         geometry_msgs::Twist msg;
@@ -49,6 +55,7 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
     ros::Subscriber alvar_sub = n.subscribe<ar_track_alvar_msgs::AlvarMarkers>("/ar_pose_marker", 1000, arposecallback);
     velocity_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1);   
+    tts_pub = n.advertise<geometry_msgs::Twist>("tts_text", 1);   
     ros::spin();
 
     return 0;
